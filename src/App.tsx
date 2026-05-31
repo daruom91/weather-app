2f6fbc6641ca873b7b0f7b76b223600843578167
Mourad Riahi <mourad.riaahi@gmail.com>
2026-05-31 12:24:16 +0100

    style: fix import case for modal stylesheet

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
       <div className="">
         <div className="header">
           <h1 className="text-5xl text-center font-bold text-white tracking-wide hover:text-amber-200 transition-all duration-300 animate-bounce-slow [text-shadow:_2px_2px_1px_#164e63] ">
-            Tempéris
+            Tempéris DaruoM
           </h1>
         </div>
         <div className="content">
